fed1fd69e0f0be37a32e8478a8f2a2f00837009d
Emeka-Okpaleke <olisemekaokpaleke@yahoo.com>
2024-12-06 11:03:25 +0100

    first commit

---
+++ src/layouts/RootLayouts.js
@@ -0,0 +1,22 @@
+import { Outlet, NavLink } from "react-router-dom";
+import Breadcrumbs from "../pages/components/Breadcrumbs";
+
+export default function RootLayout() {
+  return (
+    <div className="root-layout">
+      <header>
+        <nav>
+          <h1>Jobarouter</h1>
+          <NavLink to="/">Home</NavLink>
+          <NavLink to="about">About</NavLink>
+          <NavLink to="help">Help</NavLink>
+          <NavLink to="career">Career</NavLink>
+        </nav>
+        <Breadcrumbs/>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </div>
+  )
+}

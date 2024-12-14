7722247c29370ab35ecb131775f7d26915513693
Emeka-Okpaleke <olisemekaokpaleke@yahoo.com>
2024-12-14 22:15:20 +0100

    teting the new branch created by changing lorem ipsum in about page

--- src/pages/About.js
+++ src/pages/About.js
@@ -20,7 +20,7 @@ export default function About() {
         </p>
         <p>
           Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui provident consequuntur vel omnis quisquam rem harum,
-           maxime expedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.
+           maxime eexpedita, ullam ut dolore! Distinctio eos minima voluptatum totam id hic! Sapiente debitis quia illum officia obcaecati provident nulla odio molestiae suscipit quasi.
         </p>
         <button onClick={() => setUser(null)}>Logout</button>
       </div>
